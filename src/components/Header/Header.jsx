8d6c1727bcdc8acce6ea79287b5ec0ba0fc56faa
mitchel noble <mitchelparkernoble@gmail.com>
2021-02-07 17:00:21 -0800

    split header into header and nav components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,11 +4,9 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <div className="header-container">
+      <Link to="/">
       <h1 className="myname">Mitchel Noble</h1>
-      <Link to="/">Home</Link>
-      <Link to="/About">About</Link>
-      <Link to="/Projects">Projects</Link>
-      <Link to="/Contact">Contact</Link>
+      </Link>
     </div>
   )
 }
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from '../Header/Header'
+import Nav from '../Nav/Nav'
 import Footer from '../Footer/Footer'
 
 export default function Layout() {
   return (
     <div className="layout-container">
       <Header />
-      
+      <Nav />
       <Footer />
     </div>
   )
