2982bc4f89fa62eb8b93c1960e8a9f50816e6144
mitchel noble <mitchelparkernoble@gmail.com>
2021-02-07 17:04:06 -0800

    added css files for components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="footer-container">
-      
+    <p>Icon</p>
+    <p>Icon</p>
+    <p>Icon</p>
+    <p>Icon</p>
+    <p>Icon</p>
     </div>
   )
 }
