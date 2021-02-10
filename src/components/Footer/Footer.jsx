09ef3b274fd6b00fd38ec2d37fbabdb5fdb488af
mitchel noble <mitchelparkernoble@gmail.com>
2021-02-09 23:38:42 -0800

    fixing white space on bottom of site

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faGithubSquare,
+} from "@fortawesome/free-brands-svg-icons";
+import {faEnvelopeSquare} from "@fortawesome/free-solid-svg-icons";
+import '../../styles/components/Footer.css'
 
 export default function Footer() {
   return (
     <div className="footer-container">
-    <p>Icon</p>
-    <p>Icon</p>
-    <p>Icon</p>
-    <p>Icon</p>
-    <p>Icon</p>
+      <FontAwesomeIcon className="linkedin-icon" icon={faLinkedin} />
+      <FontAwesomeIcon className="github-icon" icon={faGithubSquare} />
+      <FontAwesomeIcon className="email-icon" icon={faEnvelopeSquare} />
     </div>
   )
 }
