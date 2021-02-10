92c66fb5c47825b9ec2ce27f169c617fa4971dec
mitchel noble <mitchelparkernoble@gmail.com>
2021-02-10 01:15:53 -0800

    changed font for projects screen

--- src/components/ProjectDetail/ProjectDetail.jsx
+++ src/components/ProjectDetail/ProjectDetail.jsx
@@ -1,15 +1,24 @@
 import React from "react";
+import '../../styles/screens/ProjectDetail.css';
 
 export default function ProjectDetail() {
   return (
     <div className="projects-container">
+      <div className="pppc-project">
       <h1>Play Price PC</h1>
+      </div>
 
+      <div className="ppp-project">
       <h1>Practice Practice Practice</h1>
+      </div>
 
+      <div className="gl-project">
       <h1>Get Lost</h1>
+      </div>
 
+      <div className="pr-project">
       <h1>Protozoa Records</h1>
+      </div>
     </div>
   );
 }
