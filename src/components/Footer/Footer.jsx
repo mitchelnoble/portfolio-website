cb201df3d9e2f87e14bae3c75cc54f72d7498b80
mitchel noble <mitchelparkernoble@gmail.com>
2021-02-10 02:54:06 -0800

    adjusted about me section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,16 +4,23 @@ import {
   faLinkedin,
   faGithubSquare,
 } from "@fortawesome/free-brands-svg-icons";
-import {faEnvelopeSquare} from "@fortawesome/free-solid-svg-icons";
+import { faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
+import { Link } from 'react-router-dom';
 import '../../styles/components/Footer.css'
 
 export default function Footer() {
   return (
     <>
-    <div className="footer-container">
-      <FontAwesomeIcon className="linkedin-icon" icon={faLinkedin} />
-      <FontAwesomeIcon className="github-icon" icon={faGithubSquare} />
+      <div className="footer-container">
+        <a href="https://www.linkedin.com/in/mitchel-noble/" className="linkedin-icon-link" target="blank">
+        <FontAwesomeIcon className="linkedin-icon" icon={faLinkedin} />
+        </a>
+        <a href="https://github.com/mitchelnoble" className="github-icon-link" target="blank">
+        <FontAwesomeIcon className="github-icon" icon={faGithubSquare} />
+        </a>
+        <Link to="/Contact" className="email-icon-link">
       <FontAwesomeIcon className="email-icon" icon={faEnvelopeSquare} />
+        </Link>
       </div>
       </>
   )
--- src/screens/Contact/Contact.jsx
+++ src/screens/Contact/Contact.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import '../../styles/screens/Contact.css'
 
 export default function Contact() {
   return (
     <div className="contact-form">
+
+      <h1 className="contact-me-title">Contact Me</h1>
+
+      <div className="information-container">
       <p>Email: MitchelParkerNoble@gmail.com</p>
       <p>LinkedIn: https://www.linkedin.com/in/mitchel-noble/ </p>  
       <p>Github: https://github.com/mitchelnoble</p>
+      </div>
+      
     </div>
   )
 }
