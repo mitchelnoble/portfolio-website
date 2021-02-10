8fafc811a84bbeb33a798ee9cd6a88ebbc73c506
mitchel noble <mitchelparkernoble@gmail.com>
2021-02-10 01:58:19 -0800

    adjusted footer icon size and styled about me section

--- src/screens/About/About.jsx
+++ src/screens/About/About.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import '../../styles/screens/About.css'
 
 export default function About() {
   return (
     <div className="about-container">
-      <p>I am a semi-professional musician and Software Engineer. Creative, sincere, and reliable, I channel the same passion that produces and performs music into programming full-stack applications. I seek the opportunity to join an innovative company to grow my skills as a Software Engineer.</p>
+      
+      <div className="title-container">
+      <h2 className="about-me-title">About Me</h2>
+      </div>
+
+      <div className="text-container">
+      <p className="about-me-text">I am a semi-professional musician and Software Engineer. Creative, sincere, and reliable, I channel the same passion that produces and performs music into programming full-stack applications. I seek the opportunity to join an innovative company to grow my skills as a Software Engineer.</p>
+      </div>
+      
     </div>
   )
 }
