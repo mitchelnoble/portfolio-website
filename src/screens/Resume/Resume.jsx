187f6595869b572d098000bd78c129efcbd42f47
mitchel noble <mitchelparkernoble@gmail.com>
2021-02-27 02:37:29 -0800

    adjusted nav animation timing

--- src/screens/Resume/Resume.jsx
+++ src/screens/Resume/Resume.jsx
@@ -5,7 +5,7 @@ export default function Resume() {
   return (
     <>
       <a href="https://www.docdroid.net/oPbR6eh/mitchel-noble-resume-pdf" target="blank">
-      <img src="https://i.imgur.com/SIU7ncC.jpg" alt="Mitchel Noble Resume" className="resume-image"></img>
+      <img src="https://i.imgur.com/0zAw2yq.png" alt="Mitchel Noble Resume" className="resume-image"></img>
       </a>
     </>
   )
