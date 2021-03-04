52643c90a60d905a7e2cfad85dfd3f3104ac663e
mitchel noble <mitchelparkernoble@gmail.com>
2021-03-03 16:09:01 -0800

    added styling for music screen

--- src/screens/Music/Music.jsx
+++ src/screens/Music/Music.jsx
@@ -7,12 +7,13 @@ export default function Music() {
     <div className="music-container">
       <div className="producer-description">
         <h1 className="music-title">Music</h1>
-        <h3 className="music-description">Under the moniker "Lucental", I have produced for a diverse variety of clients in LA, Chicago, and New York ranging from local artists to an off-broadway production.</h3>
+        <h3 className="music-description">Under the moniker "Lucental", I have produced original music for a diverse variety of clients in LA, Chicago, and New York ranging from local artists to an off-broadway production.</h3>
         <ReactPlayer
           url="https://soundcloud.com/mitchelparkernoble/sets/for-games"
           className="soundcloud-react-player"
           width="100%"
         />
+        <br />
       <iframe title="My Spotify Player" src="https://open.spotify.com/embed/playlist/4nB2tGIoaXxRoNoqEdd4R5" width="100%" height="250" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
       </div>
     </div>
