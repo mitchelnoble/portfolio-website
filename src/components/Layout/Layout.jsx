445350d4f7b793d228872215dc62463b907b7b35
mitchel noble <mitchelparkernoble@gmail.com>
2021-03-17 01:10:31 -0700

    added styling to layout component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,24 @@
 import React from "react";
+import '../../styles/components/Layout.css'
 import Header from "../Header/Header";
 import Nav from "../Nav/Nav";
 import Footer from "../Footer/Footer";
 
 export default function Layout(props) {
   return (
-    <>
+    <div className="layout-container">
+      <div className="layout-header">
       <Header />
+      </div>
+      <div className="layout-nav">
       <Nav />
-      <div>{props.children}</div>
+      </div>
+      <div className="layout-content">
+        {props.children}
+      </div>
+      <div className="layout-footer">
       <Footer />
-    </>
+      </div>
+    </div>
   );
 }
