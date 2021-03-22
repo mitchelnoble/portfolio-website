acaa1e56efbd5253b19717d78ab87e7f45d27580
mitchel noble <mitchelparkernoble@gmail.com>
2021-03-22 16:01:03 -0700

    added a tag to description

--- src/components/ProjectDetail/ProjectDetail.jsx
+++ src/components/ProjectDetail/ProjectDetail.jsx
@@ -17,7 +17,11 @@ const ProjectDetail = () => {
         <p className="pppc-description">
           <h3>Play Price PC</h3> <hr /> A web application built with solely
           HTML, CSS, and Javascript that highlights DOM manipulation and pulling
-          data from third-party API's.
+          data from third-party API's. <br /> <br /> <br /> Click{" "}
+          <a href="https://play-price-pc.netlify.app/" target="blank">
+            here
+          </a>{" "}
+          or the button below for the live application:
         </p>
       </div>
 
@@ -50,6 +54,11 @@ const ProjectDetail = () => {
           CRUD functionality, authorization, and a shopping cart system using
           local storage. Deployed to Netlify and Heroku, and built with a team
           of 3 other individuals using the Github "Gitflow" model.
+          <br /> <br /> Click{" "}
+          <a href="https://get-lost.netlify.app/" target="blank">
+            here
+          </a>{" "}
+          or the button below for the live application:
         </p>
       </div>
 
@@ -82,6 +91,11 @@ const ProjectDetail = () => {
           application developed with React client-side and Ruby on Rails
           server-side. Along with responsive styling, the site also features
           front-end and back-end Authentication.
+          <br /> <br /> Click{" "}
+          <a href="https://protozoa-records.netlify.app" target="blank">
+            here
+          </a>{" "}
+          or the button below for the live application:
         </p>
       </div>
 
