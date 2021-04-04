da0f7519eb03c18ba70ad452409148995e03c1a4
mitchel noble <mitchelparkernoble@gmail.com>
2021-04-04 14:41:17 -0700

    grid

--- src/screens/About/About.jsx
+++ src/screens/About/About.jsx
@@ -16,23 +16,24 @@ const useStyles = makeStyles((theme) => ({
 
 export default function About() {
   return (
-    <div className="about-container">
+    <>
       <div className="title-container">
         <h2 className="about-me-title">About Me</h2>
       </div>
 
       <div className="text-container">
         <p className="about-me-text">
-          I am a professional musician and Software Engineer. Creative,
-          sincere, and reliable, I channel the same passion that produces and
-          performs music into programming full-stack applications. I seek the
-          opportunity to join an innovative company to grow my skills as a
-          Software Engineer.
+          I am a professional musician and Software Engineer. Creative, sincere,
+          and reliable, I channel the same passion that produces and performs
+          music into programming full-stack applications. I seek the opportunity
+          to join an innovative company to grow my skills as a Software
+          Engineer.
         </p>
       </div>
       <h1 className="icon-title">Languages & Frameworks</h1>
       <br />
-      <div className="icon-container">
+      <Grid container direction="column">
+        <Paper>
         <i class="devicon-html5-plain-wordmark colored"></i>
         <i class="devicon-css3-plain-wordmark colored"></i>
         <i class="devicon-javascript-plain colored"></i>
@@ -42,7 +43,8 @@ export default function About() {
         <i class="devicon-ruby-plain-wordmark colored"></i>
         <i class="devicon-rails-plain-wordmark colored"></i>
         <i class="devicon-postgresql-plain-wordmark colored"></i>
-      </div>
-    </div>
+        </Paper>
+        </Grid>
+    </>
   );
 }
