d425d4fe10c5aabf8cc75262c0a52d3afcfecfaa
mitchel noble <mitchelparkernoble@gmail.com>
2021-04-04 19:35:13 -0700

    converted about component to material ui

--- src/screens/About/About.jsx
+++ src/screens/About/About.jsx
@@ -1,50 +1,42 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(1),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  },
-}));
+import Typography from "@material-ui/core/Typography";
 
 export default function About() {
   return (
     <>
-      <div className="title-container">
-        <h2 className="about-me-title">About Me</h2>
-      </div>
+      <Typography variant="h3">About Me</Typography>
+      <Typography>
+        I am a professional musician and Software Engineer. Creative, sincere,
+        and reliable, I channel the same passion that produces and performs
+        music into programming full-stack applications. I seek the opportunity
+        to join an innovative company to grow my skills as a Software Engineer.
+      </Typography>
+      <Typography variant="h3">Languages & Frameworks</Typography>
 
-      <div className="text-container">
-        <p className="about-me-text">
-          I am a professional musician and Software Engineer. Creative, sincere,
-          and reliable, I channel the same passion that produces and performs
-          music into programming full-stack applications. I seek the opportunity
-          to join an innovative company to grow my skills as a Software
-          Engineer.
-        </p>
-      </div>
-      <h1 className="icon-title">Languages & Frameworks</h1>
-      <br />
       <Grid container direction="column">
-        <Paper>
-        <i class="devicon-html5-plain-wordmark colored"></i>
-        <i class="devicon-css3-plain-wordmark colored"></i>
-        <i class="devicon-javascript-plain colored"></i>
-        <i class="devicon-nodejs-plain-wordmark colored"></i>
-        <i class="devicon-express-original-wordmark colored"></i>
-        <i class="devicon-react-original-wordmark colored"></i>
-        <i class="devicon-ruby-plain-wordmark colored"></i>
-        <i class="devicon-rails-plain-wordmark colored"></i>
-        <i class="devicon-postgresql-plain-wordmark colored"></i>
-        </Paper>
-        </Grid>
+        <Typography variant="h2">
+          <i class="devicon-html5-plain-wordmark colored"></i>
+          <i class="devicon-css3-plain-wordmark colored"></i>
+          <i class="devicon-javascript-plain colored"></i>
+        </Typography>
+      </Grid>
+
+      <Grid container direction="column">
+        <Typography variant="h2">
+          <i class="devicon-nodejs-plain-wordmark colored"></i>
+          <i class="devicon-express-original-wordmark colored"></i>
+          <i class="devicon-react-original-wordmark colored"></i>
+        </Typography>
+      </Grid>
+
+      <Grid container direction="column">
+        <Typography variant="h2">
+          <i class="devicon-ruby-plain-wordmark colored"></i>
+          <i class="devicon-rails-plain-wordmark colored"></i>
+          <i class="devicon-postgresql-plain-wordmark colored"></i>
+        </Typography>
+      </Grid>
     </>
   );
 }
