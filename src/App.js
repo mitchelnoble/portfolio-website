7e7878ed551d59d1c1b0916879367d61ae13bbca
mitchel noble <mitchelparkernoble@gmail.com>
2021-04-06 20:35:48 -0700

    routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import './App.css';
-import React from 'react';
-import Link from 'react-router-dom';
-import Header from './components/Header/Header'
-import About from './screens/About/About'
-import Projects from './components/Projects/Projects'
-import Resume from './screens/Resume/Resume'
-import Contact from './screens/Contact/Contact'
-import Music from './screens/Music/Music'
+import "./App.css";
+import React from "react";
+import Header from "./components/Header/Header";
+import About from "./screens/About/About";
+import Projects from "./components/Projects/Projects";
+import Resume from "./screens/Resume/Resume";
+import Contact from "./screens/Contact/Contact";
+import Music from "./screens/Music/Music";
 
 function App() {
   return (
     <div className="App">
-        <Header />
-        <About id="About" />
-        <Projects />
-        <Resume />
-        <Contact />
-        <Music />
+      <Header />
+      <About />
+      <Projects />
+      <Resume />
+      <Contact />
+      <Music />
     </div>
   );
 }
