e3f5ad1eb3c03d4a29f30561fefc40765f69c5d1
mitchel noble <mitchelparkernoble@gmail.com>
2021-05-01 17:03:36 -0700

    working on form component styling

--- src/screens/Contact/Contact.jsx
+++ src/screens/Contact/Contact.jsx
@@ -3,6 +3,10 @@ import emailjs from "emailjs-com";
 import "../../styles/screens/Contact.css";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+import Input from "@material-ui/core/Input";
+import SendIcon from '@material-ui/icons/Send';
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles({
   button: {
@@ -35,40 +39,43 @@ export default function Contact() {
   return (
     <div id="contact">
         <br /> <br />
-        <Button variant="outlined" size="large" class={classes.button} >CONTACT</Button>
-      <div className="form-and-details">
-        <div className="contact-form">
-          <form onSubmit={sendEmail} className="the-form">
-            <input
+      <Button variant="outlined" size="large" class={classes.button} >CONTACT</Button>
+          <form className="the-form">
+      <Grid container direction="column" justify="centered">
+            <Input
               type="text"
-              className="name-box"
+
               placeholder="Name"
               name="name"
               />
-            <input
+            <Input
               type="text"
-              className="email-box"
+
               placeholder="Email"
               name="email"
               />
-            <input
+            <Input
               type="text"
-              className="subject-box"
+
               placeholder="Subject"
               name="subject"
               />
-            <textarea
-              className="text-box"
-              id=""
-              cols="80"
-              rows="12"
-              placeholder="Your Message"
-              name="message"
-              />
-            <input type="submit" className="submit-button" value="Send" />
+        <TextField
+            multiline
+            placeholder="Message"
+          rows={5}
+          />
+          <Grid item xs={1}>
+        <Button
+          onClick={() => { sendEmail() }}
+          variant="contained"
+          color="primary"
+          endIcon={<SendIcon />}>
+          Send
+          </Button>
+        </Grid>
+      </Grid>
           </form>
         </div>
-      </div>
-    </div>
   );
 }
